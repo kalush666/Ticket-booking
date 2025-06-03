c01c05883a1a4eef77a02126a3b491ffa479d2fb
kalush666 <jonathan.kalush@gmail.com>
2025-06-03 22:20:21 +0300

    tab layout

---
+++ frontend/app/(authed)/(tabs)/(events)/_layout.tsx
@@ -0,0 +1,73 @@
+import TabBarIcon from "@/components/navigation/TabBarIcon";
+import { Tabs } from "expo-router";
+import { ComponentProps } from "react";
+import { Text } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default function TabLayout() {
+  const tabs = [
+    {
+      showFor: [],
+      name: "(events)",
+      displayName: "Events",
+      icon: "calendar-outline",
+      options: {
+        headerShown: false,
+      },
+    },
+    {
+      showFor: [],
+      name: "(tickets)",
+      displayName: "My Tickets",
+      icon: "ticket-outline",
+      options: {
+        headerShown: false,
+      },
+    },
+    {
+      showFor: [],
+      name: "scan-ticket",
+      displayName: "Scan Ticket",
+      icon: "scan-outline",
+      options: {
+        headerShown: true,
+      },
+    },
+    {
+      showFor: [],
+      name: "settings",
+      displayName: "Settings",
+      icon: "settings-outline",
+      options: {
+        headerShown: true,
+      },
+    },
+  ];
+
+  return (
+    <Tabs>
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            ...tab.options,
+            headerTitle: tab.displayName,
+            //href: tab.showFor.includes()
+            tabBarLabel: ({ focused }) => (
+              <Text style={{ color: focused ? "black" : "gray" }}>
+                {tab.displayName}
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                name={tab.icon as ComponentProps<typeof Ionicons>["name"]}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+}
