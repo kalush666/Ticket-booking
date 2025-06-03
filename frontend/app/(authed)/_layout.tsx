3b04f9124d52604b495d74bf0dd0ba062ed975d9
kalush666 <jonathan.kalush@gmail.com>
2025-06-03 22:04:29 +0300

    app layout

--- frontend/app/(authed)/_layout.tsx
+++ frontend/app/(authed)/_layout.tsx
@@ -1,1 +1,11 @@
-export default function AppLayout() {}
+import { Redirect, Slot } from "expo-router";
+
+export default function AppLayout() {
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return <Redirect href="../login" />;
+  }
+
+  return <Slot />;
+}
